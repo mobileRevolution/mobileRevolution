dce229244a2a4a3235e45cef5acf9b0520feec3d
Artem <artem.abramov@simbirsoft.com>
2017-10-03 17:33:51 +0400

    Minor code review

--- app/src/main/java/com/example/dmitry/mobilerevolution/ElementActivity.java
+++ app/src/main/java/com/example/dmitry/mobilerevolution/ElementActivity.java
@@ -10,10 +10,17 @@ import android.widget.TextView;
  */
 
 public class ElementActivity  extends AppCompatActivity {
+    /*private static final String BUNDLE_ID = "bundle_id";*/
 
     private TextView nameOfProduct;
     private ImageView image;
     private TextView description;
+
+    /*public static Intent createStartIntent(Context context, String id) {
+        Intent intent = new Intent(context, ElementActivity.class);
+        intent.putExtra(BUNDLE_ID, id);
+        return intent;
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,7 @@ public class ElementActivity  extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
 
-
+        // Этот код выносится в отдельный метод, названия полей задаются константами
         image.setImageBitmap((Bitmap) extras.getParcelable("photoOfProduct"));
         nameOfProduct.setText(extras.getString("nameOfProduct"));
         description.setText(extras.getString("descriptionOfProduct"));
