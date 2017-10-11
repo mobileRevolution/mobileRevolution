32aeacbb57b1a04f134ffc7c17fec695245eb443
Artem <artem.abramov@simbirsoft.com>
2017-10-11 10:59:12 +0400

    Added API tokens

---
+++ app/src/main/java/com/example/dmitry/mobilerevolution/api/AccessInterface.java
@@ -0,0 +1,8 @@
+package com.example.dmitry.mobilerevolution.api;
+
+public interface AccessInterface {
+    String HEADERs =
+            "Authorization: Basic NTlkZGI5ZmZhNGJlMjA1ODZkMjYwZWEyOiBhMDUzMTMxN2Jk" +
+            "X-Api-Factory-Application-Id: 59ddb9ffa4be20586d260ea2" +
+            "Content-Type: application/json";
+}
